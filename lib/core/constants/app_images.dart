ec46670c780704056bf3df682b78e96392aac5ec
SHYAM <syamkumar@MacBook-Air.local>
2026-01-12 11:45:45 +0530

    spash_logo

--- lib/core/constants/app_images.dart
+++ lib/core/constants/app_images.dart
@@ -1,4 +1,5 @@
 class AppImages {
   static const String logo = "assets/images/pixora_app_icon.png";
   static const String girl = "assets/images/girl.jpg";
+  static const String splashIcon = "assets/images/splash_icon.png";
 }
